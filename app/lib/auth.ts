eb429cc67075999c782c7d30039680e96a66ef43
simpleitis <amarkrishna25@gmail.com>
2026-06-11 12:46:25 +0530

    add in passkeys package

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/app/lib/db";
 import * as schema from "@/app/lib/db/schema";
+import { passkey } from "@better-auth/passkey";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
@@ -17,4 +18,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [passkey()],
 });
