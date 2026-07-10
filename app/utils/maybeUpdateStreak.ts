c8b0a7687492778b7cc39947615fbd38be9c0d27
simpleitis <amarkrishna25@gmail.com>
2026-07-10 11:57:19 +0530

    add in streak update helper function

---
+++ app/utils/maybeUpdateStreak.ts
@@ -0,0 +1,12 @@
+import { updateStreak } from "@/app/lib/streak/update-streak";
+
+export async function maybeUpdateStreak({
+  user,
+}: {
+  user: { id: string; lastActivityDate: string | null };
+}) {
+  const today = new Date().toISOString().split("T")[0];
+  if (user.lastActivityDate !== today) {
+    await updateStreak({ userId: user.id });
+  }
+}
