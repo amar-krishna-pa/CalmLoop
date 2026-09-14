cb9a21bfdc5fcabf50b067a96f128ce5fdc3e27b
simpleitis <amarkrishna25@gmail.com>
2026-09-14 12:41:23 +0530

    align login button to the center

--- app/components/auth/AuthButton.tsx
+++ app/components/auth/AuthButton.tsx
@@ -21,7 +21,7 @@ export default function AuthButton({
       disabled={loading !== null}
       className="w-80 border border-subtle bg-card hover:bg-surface rounded-lg px-4 py-2.5 text-sm font-medium text-primary flex justify-center cursor-pointer"
     >
-      <div className="w-50 grid grid-cols-[20px_1fr] items-center gap-3 ml-10">
+      <div className="flex items-center gap-3">
         <span className="w-5 h-5 flex items-center justify-center">
           {loading == method ? <LoadingSpinner /> : icon}
         </span>
